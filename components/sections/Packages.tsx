fad9cdb2f914f205ffd808a7b1898a4353194c2c
gilbertmunuotz <officialgilbert24@gmail.com>
2025-08-18 08:50:00 +0300

    #feat: updates on website footer and packages cards

--- components/sections/Packages.tsx
+++ components/sections/Packages.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+'use client'
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
+import { Button } from "../ui/button"
 
 export default function Packages() {
   return (
-    <div>Packages</div>
+    <div className="max-w-7xl mx-auto px-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-20 my-14 px-28">
+
+        <Card className="bg-white/10 backdrop-blur-md border border-white/20 shadow-lg">
+          <CardHeader>
+            <CardTitle className="text-white text-center text-2xl">Standard Package</CardTitle>
+          </CardHeader>
+          <CardContent className="text-white">
+            <li>Post-Conference Resources</li>
+            <li>Access All Session</li>
+            <li>Conference Materials</li>
+            <li>Network Opportunities</li>
+            <li>Exclusive Exhibitions</li>
+            <li>Meals & Refreshments</li>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button variant='outline' className="cursor-pointer bg-[#FBAE04] rounded-none">
+              <p className="justify-center p-5">Book</p>
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="bg-white/10 backdrop-blur-md border border-white/20 shadow-lg">
+          <CardHeader>
+            <CardTitle className="text-white text-center text-2xl">Premium Package</CardTitle>
+          </CardHeader>
+          <CardContent className="text-white">
+            <li>Post-Conference Resources</li>
+            <li>Access All Session</li >
+            <li>Workshop Certificate</li >
+            <li>Exclusive Exhibitions</li >
+            <li>Meals & Refreshments</li >
+            <li>Executive Networking Dinner</li >
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button variant="outline" className="cursor-pointer bg-[#FBAE04] rounded-none">
+              <p className="justify-center p-4">Book</p>
+            </Button>
+          </CardFooter>
+        </Card>
+
+      </div>
+    </div>
   )
 }
